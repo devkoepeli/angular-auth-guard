f922539b05eb01562f834eca16634086aae1ebad
devkoepeli <devrim_kopeli@hotmail.com>
2024-03-31 10:02:26 +0200

    feat: add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductPageComponent } from './components/product-page/product-page.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 export const routes: Routes = [
-    { path: '', component: ProductPageComponent }
+    { path: '', component: LoginComponent },
+    { path: 'Products', component: ProductPageComponent }
 ];
