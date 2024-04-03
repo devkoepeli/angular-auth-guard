b03cb50cdedc8c3f0dd0356c806189695c48f0b2
devkoepeli <devrim_kopeli@hotmail.com>
2024-04-03 17:51:03 +0200

    feat: add login form validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AbstractControl, FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
-  submitForm() {
-    console.log("form was submitted");
+  formBuilder = inject(FormBuilder);
+
+  isEmailFocused = false;
+  isPasswordFocused = false;
+  isFormSubmitted = false;
+
+  loginForm = this.formBuilder.group({
+    email: ['', [
+      Validators.required,
+      Validators.pattern('[^@]+@[^\.]+\..+')
+    ]],
+    password: ['', [
+      Validators.required,
+      Validators.pattern('(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}')
+    ]]
+  })
+
+  submitForm(): void {
+    this.isFormSubmitted = true;
+    if (this.loginForm.valid) {
+      this.email?.disable();
+      this.password?.disable();
+
+      this.email?.enable();
+      this.password?.enable();
+      this.isFormSubmitted = false;
+    }
   }
 
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 }
